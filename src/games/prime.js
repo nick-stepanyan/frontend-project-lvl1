e7555901b4fe7e9acbdc7d95f69879922714b82f
nick-stepanyan <nick.win.32@mail.ru>
2022-04-01 22:38:54 +0300

    0 не является простым или каким-то другим числом, я исправил диапазон, чтобы оно не предлагалось в тесте

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,52 +2,52 @@ import readlineSync from 'readline-sync';
 import welcomeStart from '../index.js';
 
 const primeFunk = () => {
-  const name = welcomeStart();
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+    const name = welcomeStart();
+    console.log(`Hello, ${name}!`);
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  const mainLogig = () => {
-    // const name = askName();
-    let step = 0; // number of rounds
-    let result = `Congratulations, ${name}!`;
-    let trueAnswer;
-    const getRandomInt = (max) => Math.floor(Math.random() * max);
+    const mainLogig = () => {
+        // const name = askName();
+        let step = 0; // number of rounds
+        let result = `Congratulations, ${name}!`;
+        let trueAnswer;
+        const getRandomInt = (max) => Math.floor((Math.random() * max) + 1);
 
-    const Prime = (x) => {
-      let i = 0;
-      let b = 0;
-      while (i !== x) {
-        i += 1;
-        if (x % i === 0) {
-          b += 1;
+        const Prime = (x) => {
+            let i = 0;
+            let b = 0;
+            while (i !== x) {
+                i += 1;
+                if (x % i === 0) {
+                    b += 1;
+                }
+            }
+            if (b < 3) {
+                trueAnswer = 'yes';
+            }
+            if (b > 2) {
+                trueAnswer = 'no';
+            }
+            return trueAnswer;
+        };
+        while (step !== 3) {
+            const x = getRandomInt(100);
+            const number = x; // number generating
+            Prime(number);
+            console.log(`Question: ${number}`);
+            const answer = readlineSync.question('Your answer: '); // answer
+            step += 1;
+            if (answer === trueAnswer) {
+                console.log('Correct!');
+            } else if (answer !== trueAnswer) {
+                result = (`${answer} is wrong answer ;(. Correct answer was ${trueAnswer}. Lets try again, ${name}!`);
+                break;
+            }
         }
-      }
-      if (b < 3) {
-        trueAnswer = 'yes';
-      }
-      if (b > 2) {
-        trueAnswer = 'no';
-      }
-      return trueAnswer;
+        return result;
     };
-    while (step !== 3) {
-      const x = getRandomInt(101);
-      const number = x; // number generating
-      Prime(number);
-      console.log(`Question: ${number}`);
-      const answer = readlineSync.question('Your answer: '); // answer
-      step += 1;
-      if (answer === trueAnswer) {
-        console.log('Correct!');
-      } else if (answer !== trueAnswer) {
-        result = (`${answer} is wrong answer ;(. Correct answer was ${trueAnswer}. Lets try again, ${name}!`);
-        break;
-      }
-    }
-    return result;
-  };
 
-  console.log(mainLogig());
+    console.log(mainLogig());
 };
 
 export default primeFunk;
